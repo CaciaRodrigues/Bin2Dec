99ceb433db6334553411ea8a0a3713fd53e08109
CaciaRodrigues <caciarodriguues@gmail.com>
2023-07-27 17:16:26 -0300

    Criando o evento de submit que envia o valor do input para o campo adequado.

---
+++ script.js
@@ -0,0 +1,22 @@
+const binaryInputBox = document.getElementById('binary-inputbox'); //Pegando o elemento de input do HTML pelo ID
+const decimalResult = document.getElementById('decimal-equiv'); //Pegando o destino do input do HTML pelo ID
+
+// Evento de submit que envia o valor digitado no input para o campo do decimal
+
+binaryInputBox.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let binaryNumber = e.target.elements['binary-input'];
+    
+    decimalResult.innerHTML = binaryNumber.value;
+
+    //transformNumber(binaryNumber);
+        
+    console.log(binaryNumber.value); 
+
+    binaryNumber.value = "";
+})
+
+/*function transformNumber(number) {
+    decimalResult.innerHTML = number.value;
+}*/
