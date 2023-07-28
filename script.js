c51291b19a313f8067b55dbccc7ca8049355b6c6
CaciaRodrigues <caciarodriguues@gmail.com>
2023-07-28 15:56:19 -0300

    Refatoração do código das mensagens de erro

--- script.js
+++ script.js
@@ -1,40 +1,35 @@
-const binaryInputBox = document.getElementById('binary-inputbox'); //Pegando o elemento de input do HTML pelo ID
-const decimalResult = document.getElementById('decimal-equiv'); //Pegando o destino do input do HTML pelo ID
-const transformBox = document.getElementById('box');
+const binaryInputBox = document.getElementById('binary-inputbox'); // Elemento de input do HTML pelo ID
+const decimalResult = document.getElementById('decimal-equiv'); // Destino do input do HTML pelo ID
+const mensagemErro = document.getElementById('invalid_message'); // Campo da mensagem de erro
 
 // Evento de submit que envia o valor digitado no input para o campo do decimal
-
 binaryInputBox.addEventListener('submit', (e) => {
     e.preventDefault();
 
     let binaryNumber = e.target.elements['binary-input'];
-    
-    // decimalResult.innerHTML = binaryNumber.value;
 
     transformNumber(binaryNumber);
         
     console.log(binaryNumber.value); 
 
     binaryNumber.value = "";
-    decimalResult.value = "";
 })
 
 function transformNumber(number) {
     // Caso o input tenha mais do que 8 dígitos
     if(number.value.length > 8) {
-        const invalidLengthMessage = document.createElement('p');
-        invalidLengthMessage.classList.add('invalid_message');
-        invalidLengthMessage.innerHTML = 'Your entry exceeds our limit of 8 digits! Please try again.';
-        transformBox.appendChild(invalidLengthMessage);
+        mensagemErro.style.display = "block";
+        mensagemErro.innerHTML = 'Your entry exceeds our limit of 8 digits! Please try again.';
+        decimalResult.innerHTML = '<i class="fa-solid fa-circle-exclamation"></i>'
     // Caso o input seja diferente de um número binário
     } else if(/[^01]/.test(Number(number.value))){
-        const invalidDigMessage = document.createElement('p');
-        invalidDigMessage.classList.add('invalid_message');
-        invalidDigMessage.innerHTML = 'Your entry is not a binary number! Please enter only binary digits.';
-        transformBox.appendChild(invalidDigMessage);
+        mensagemErro.style.display = "block";
+        mensagemErro.innerHTML = 'Your entry is not a binary number! Please enter only binary digits.';
+        decimalResult.innerHTML = '<i class="fa-solid fa-circle-exclamation"></i>'
     // Transformando o valor binário em decimal e o colocando dentro do innerHTML do decimal. 
     } else {
         decimalResult.innerHTML = parseInt(number.value, 2);
+        mensagemErro.style.display = "none";
      }  
 }
          
